f3e6228f2d604df770cd0b6d6facda34fadd1fff
Angie Katherine Toro Echeverry <angie.toro@globant.com>
2024-02-28 16:05:26 -0500

    add api request

--- bootcamp/src/components/Login/Login.jsx
+++ bootcamp/src/components/Login/Login.jsx
@@ -1,9 +1,34 @@
+import React, { useState } from 'react';
+
 
 export default function Login(){
+    const [term, setTerm] = useState('')
+    const [data, setData] = useState({});
+
+    async function search(){
+        const response = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${term}`)
+        const responseJson = await response.json()
+        console.log(responseJson)
+        setData({
+            character: responseJson[0].character,
+            quote: responseJson[0].quote,
+            image: responseJson[0].image
+        })
+    }
+
     return(
         <>
             <div>
-                <p>estoy en login</p>
+                <input type="text" placeholder="Search" onChange={(e) => setTerm(e.target.value)} />
+                <button type="submit" onClick={search}>Search</button>
+                <hr/>
+                {data.character && (
+                    <div>
+                        <img src={data.image} alt={data.character || ''} />
+                        <p>{data.character || ''}</p>
+                        <p>{data.quote || ''}</p>
+                    </div>
+                )}
             </div>
         </>
     )
